2bde2c318fb559e942ce34752f330b4726da0817
Manny <egalli64@gmail.com>
2021-01-13 12:29:17 +0100

    first rest example

---
+++ src/main/java/com/example/swr/s06/HelloCtrl.java
@@ -0,0 +1,19 @@
+package com.example.swr.s06;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class HelloCtrl {
+    private static final Logger log = LoggerFactory.getLogger(HelloCtrl.class);
+
+    @GetMapping("/hello")
+    Hello sayHello() {
+        log.trace("hello");
+        return new Hello("Hello World!");
+    }
+}
