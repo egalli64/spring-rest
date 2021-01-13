fe3673ba95fd50fea7acdc584663a3fb9b7c35e9
Manny <egalli64@gmail.com>
2021-01-13 11:02:19 +0100

    first commit

---
+++ src/test/java/com/example/swr/SwrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
