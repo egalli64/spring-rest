aec998fd342ad90e490b37a413a6f4cdf45c2534
Manny <egalli64@gmail.com>
2021-01-15 10:44:43 +0100

    delete

---
+++ src/main/java/com/example/swr/s14/CoderDeleteCtrl.java
@@ -0,0 +1,33 @@
+package com.example.swr.s14;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.swr.s08.CoderRepo;
+
+@RestController
+public class CoderDeleteCtrl {
+    private static final Logger log = LoggerFactory.getLogger(CoderDeleteCtrl.class);
+
+    private CoderRepo repo;
+
+    public CoderDeleteCtrl(CoderRepo repo) {
+        this.repo = repo;
+    }
+
+    @DeleteMapping("/s14/coders/{id}")
+    public void delete(@PathVariable Integer id) {
+        log.trace("delete " + id);
+        try {
+            repo.deleteById(id);
+        } catch (EmptyResultDataAccessException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Coder %d not found", id), ex);
+        }
+    }
+}
