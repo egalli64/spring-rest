627a6b3ef207f11bcce8c7606d7775f277181a54
Emanuele <egalli64@gmail.com>
2025-06-19 11:11:55 +0200

    ErrorController upgrade

--- src/main/java/com/example/swr/ErrorCtr.java
+++ src/main/java/com/example/swr/ErrorCtr.java
@@ -2,6 +2,7 @@ package com.example.swr;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,13 +10,9 @@ public class ErrorCtr implements ErrorController {
 
     private static final String PATH = "/error";
 
-    @RequestMapping(value = PATH)
+    @RequestMapping(value = PATH, method = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+            RequestMethod.DELETE, RequestMethod.PATCH })
     public String error() {
         return "";
-    }
-
-    @Override
-    public String getErrorPath() {
-        return PATH;
     }
 }
