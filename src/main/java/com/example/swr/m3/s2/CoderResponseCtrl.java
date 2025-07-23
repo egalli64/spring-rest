c371266c0ba707e76a4eb39d7e51bb05ad6c5ab1
Emanuele <egalli64@gmail.com>
2025-07-23 15:50:31 +0200

    ResponseEntity

---
+++ src/main/java/com/example/swr/m3/s2/CoderResponseCtrl.java
@@ -0,0 +1,64 @@
+/*
+ * A Spring Boot RESTful application 
+ * 
+ * https://github.com/egalli64/swr
+ */
+package com.example.swr.m3.s2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.swr.dao.Coder;
+import com.example.swr.dao.CoderRepo;
+
+@RestController
+@RequestMapping("/api/m3/s2/coders") // Base path for API Version 1
+public class CoderResponseCtrl {
+    private static final Logger log = LogManager.getLogger(CoderResponseCtrl.class);
+
+    private CoderRepo repo;
+
+    public CoderResponseCtrl(CoderRepo repo) {
+        this.repo = repo;
+    }
+
+    /**
+     * <pre>
+     * curl -v localhost:8080/api/m3/s2/coders
+     * </pre>
+     * 
+     * @return a response with status HTTP 200 OK
+     */
+    @GetMapping
+    public ResponseEntity<Iterable<Coder>> getAllCoders() {
+        log.traceEntry("getAll");
+        return ResponseEntity.ok(repo.findAll());
+    }
+
+    /**
+     * <pre>
+     * curl -i -X DELETE localhost:8080/api/m3/s2/coders/1
+     * </pre>
+     * 
+     * @return a response with status HTTP 204 No Content
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCoder(@PathVariable Integer id) {
+        log.trace("delete " + id);
+
+        if (!repo.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Coder %d not found", id));
+        }
+        repo.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
