3c56f661116a061ff29dde62097bb6e105dbc795
Emanuele <egalli64@gmail.com>
2025-07-24 10:04:21 +0200

    Validation support

--- src/main/java/com/example/swr/dao/Coder.java
+++ src/main/java/com/example/swr/dao/Coder.java
@@ -14,6 +14,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 /**
  * A simple entity JPA
@@ -26,9 +33,23 @@ public class Coder {
     @Column(name = "CODER_ID")
     private Integer id;
 
+    @Size(min = 2, max = 20, message = "First name must be between 2 and 20 characters")
+    @Pattern(regexp = "^[A-Za-z ]*$", message = "First name must contain only letters and spaces")
     private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    @Size(min = 2, max = 25, message = "Last name must be between 2 and 25 characters")
+    @Pattern(regexp = "^[A-Za-z ]*$", message = "Last name must contain only letters and spaces") // Updated regex
+    @Column(nullable = false)
     private String lastName;
+
+    @NotNull(message = "Hire date is mandatory")
+    @PastOrPresent(message = "Hire date cannot be in the future")
+    @Column(nullable = false)
     private LocalDate hireDate;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "Salary must be greater than 0")
+    @Digits(integer = 8, fraction = 2, message = "Salary has max 8 digits, 2 decimal places")
     private BigDecimal salary;
 
     public Coder() {
