f54ba2b5d0f39ec5d07cf2ad768596fca491d22c
Emanuele <egalli64@gmail.com>
2025-09-12 11:57:14 +0200

    Stable pagination support

--- src/main/java/com/example/swr/SwrApplication.java
+++ src/main/java/com/example/swr/SwrApplication.java
@@ -7,8 +7,10 @@ package com.example.swr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SwrApplication {
     public static void main(String[] args) {
         SpringApplication.run(SwrApplication.class, args);
