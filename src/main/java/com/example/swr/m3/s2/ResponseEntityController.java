8f14d1f0d830b837918fe4e1791a72f5e60c1e0f
Emanuele <egalli64@gmail.com>
2025-09-12 10:56:59 +0200

    review

--- src/main/java/com/example/swr/m3/s2/ResponseEntityController.java
+++ src/main/java/com/example/swr/m3/s2/ResponseEntityController.java
@@ -5,17 +5,18 @@
  */
 package com.example.swr.m3.s2;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
+import com.example.swr.exception.CoderNotFoundException;
 import com.example.swr.model.Coder;
 import com.example.swr.repository.CoderRepository;
 
@@ -24,31 +25,31 @@ import com.example.swr.repository.CoderRepository;
  */
 @RestController
 @RequestMapping("/api/m3/s2/coders")
-public class CoderResponseCtrl {
-    private static final Logger log = LogManager.getLogger(CoderResponseCtrl.class);
+public class ResponseEntityController {
+    private static final Logger log = LogManager.getLogger(ResponseEntityController.class);
 
     private CoderRepository repo;
 
-    public CoderResponseCtrl(CoderRepository repo) {
+    public ResponseEntityController(CoderRepository repo) {
         this.repo = repo;
     }
 
     /**
      * <pre>
-     * curl -v localhost:8080/api/m3/s2/coders
+         curl -v localhost:8080/api/m3/s2/coders
      * </pre>
      * 
      * @return a response with status HTTP 200 OK
      */
     @GetMapping
-    public ResponseEntity<Iterable<Coder>> getAllCoders() {
+    public ResponseEntity<List<Coder>> getAllCoders() {
         log.traceEntry("getAll");
         return ResponseEntity.ok(repo.findAll());
     }
 
     /**
      * <pre>
-     * curl -i -X DELETE localhost:8080/api/m3/s2/coders/1
+         curl -i -X DELETE localhost:8080/api/m3/s2/coders/1
      * </pre>
      * 
      * @return a response with status HTTP 204 No Content
@@ -58,7 +59,7 @@ public class CoderResponseCtrl {
         log.trace("delete " + id);
 
         if (!repo.existsById(id)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Coder %d not found", id));
+            throw new CoderNotFoundException(id);
         }
         repo.deleteById(id);
 
